4d9e71aa7b896e5427bdebaa4a88aa4adca6444a
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-03-20 22:58:54 +0000

    work

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: dominik armatys
-// Created on: mar 20 2023
+// Created on: Sep 2020
 // This file contains the JS functions for index.html
 
 /**
@@ -9,10 +9,10 @@
  */
 function enterClicked() {
   // input
-  const firstName = document.getElementById("streetx-name").value
-  const userAge = parseInt(document.getElementById("streetx-number").value)
+  const firstName = document.getElementById("street-name").value
+  const userAge = parseInt(document.getElementById("street-number").value)
 
   // output
   document.getElementById("address").innerHTML =
-    "Your info is: " + firstName + ", age " + userAge + "."
+    "Your adress is: " + firstName + " street, number " + userAge + "."
 }
---
+++ userInput.go
@@ -0,0 +1,23 @@
+// Created by: Dominik Armatys
+// Created on: mar 20 2023
+//
+// This program does addition
+
+package main
+
+import "fmt"
+
+func main() {
+	// This function does addition
+	var firstName string
+	var age int
+
+	// input
+	fmt.Println("This program gets a user's street and number.")
+	fmt.Println()
+	fmt.Print("Enter your street name: ")
+	fmt.Scanln(&street)
+	fmt.Print("Enter your street number: ")
+	fmt.Scanln(&number)
+
+	
