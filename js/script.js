d7ae8a4546d4ad5273fd70fb8768dd87883441ec
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-03-20 17:24:36 +0000

    i love boys but not gay

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: dominik armatys
+// Created on: mar 20 2023
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("streetx-name").value
+  const userAge = parseInt(document.getElementById("streetx-number").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your info is: " + firstName + ", age " + userAge + "."
+}
